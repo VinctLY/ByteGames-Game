6d133a4ef0b2dd195b93d2b789bfe827e0332212
Vin <vincenty1004@gmail.com>
2024-05-05 20:42:13 +0700

    add: new games page

---
+++ client/src/app/games/page.tsx
@@ -0,0 +1,5 @@
+import { minecraft } from "../layout";
+
+export default function page() {
+	return <main className={`${minecraft.className} min-h-screen p-8 flex justify-center items-center`}>No games for now..</main>;
+}
