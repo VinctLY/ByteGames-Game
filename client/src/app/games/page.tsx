678d3a046b74c747bdf937d743098b989300739e
Vin <vincenty1004@gmail.com>
2024-05-07 11:46:57 +0700

    fix: change pages metadata

--- client/src/app/games/page.tsx
+++ client/src/app/games/page.tsx
@@ -1,5 +1,16 @@
+import { Metadata } from "next";
+
 import { minecraft } from "../layout";
 
+export const metadata: Metadata = {
+	title: "Find Games",
+	description: "Find various games by ByteGames developer",
+};
+
 export default function page() {
-	return <main className={`${minecraft.className} min-h-screen p-8 flex justify-center items-center`}>No games for now..</main>;
+	return (
+		<main className={`${minecraft.className} min-h-screen p-8 flex justify-center items-center`}>
+			No games for now..
+		</main>
+	);
 }
