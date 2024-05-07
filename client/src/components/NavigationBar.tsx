26398aa869a7d4798f4cc420dff6543641625b85
Vin <vincenty1004@gmail.com>
2024-05-07 10:28:47 +0700

    fix: add more navigation bar functionality and fix unintentional behaviour   - navigation close on click outside the area   - navigation close on window resize (>640px)   - stop website from scrolling when navigation is open   - navigation links color change on hover   - lnavigation links interface

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -5,12 +5,18 @@ import Link from "next/link";
 import localFont from "next/font/local";
 
 import Logo from "/public/ByteGames.png";
-import NavigationLinks from "./NavigationLinks";
+import NavigationLinks, { link } from "./NavigationLinks";
 
 const minecraft = localFont({
 	src: "../assets/fonts/Minecraft.otf",
 	fallback: ["sans-serif"],
 });
+
+const links: link[] = [
+	{ anchor: "About", url: "/about" },
+	{ anchor: "Games", url: "/games" },
+	{ anchor: "Visit Github", url: "https://github.com/VinctLY" },
+];
 
 export default function NavigationBar() {
 	return (
@@ -19,7 +25,7 @@ export default function NavigationBar() {
 				<Link href="/">
 					<Image src={Logo} alt="ByteGames logo" className="w-[144px]" />
 				</Link>
-				<NavigationLinks />
+				<NavigationLinks links={links} />
 			</nav>
 		</header>
 	);
--- client/src/components/NavigationLinks.tsx
+++ client/src/components/NavigationLinks.tsx
@@ -4,31 +4,75 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
 import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-export default function NavigationLinks() {
+export interface link {
+	url: string;
+	anchor: string;
+}
+
+interface NavigationLinksProps {
+	links: link[];
+}
+
+export default function NavigationLinks({ links }: NavigationLinksProps) {
 	let [navOpen, setNavOpen] = useState(false);
+	let navRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		if (navOpen) document.body.style.overflowY = "hidden";
+		else document.body.style.overflowY = "visible";
+
+		function onclick(evt: MouseEvent) {
+			if (navRef.current) {
+				const width = navRef.current.offsetWidth;
+				const pos_from_right = document.body.offsetWidth - evt.clientX;
+
+				if (pos_from_right > width) setNavOpen(false);
+			}
+		}
+
+		document.body.addEventListener("click", onclick);
+
+		return () => document.body.removeEventListener("click", onclick);
+	}, [navOpen]);
+
+	useEffect(() => {
+		function onresize() {
+			if (document.body.clientWidth > 640) setNavOpen(false);
+		}
+
+		window.addEventListener("resize", onresize);
+
+		return () => {
+			window.removeEventListener("resize", onresize);
+		};
+	}, []);
 
 	return (
-		<ul>
+		<ul className="flex items-center">
 			<FontAwesomeIcon
-				icon={navOpen ? faXmark : faBars}
-				className="absolute top-4 right-8 size-6 cursor-pointer z-10 sm:hidden"
-				onClick={() => setNavOpen(!navOpen)}
+				icon={faBars}
+				className="sm:hidden size-6 cursor-pointer"
+				onClick={() => setNavOpen(true)}
 			/>
 			<div
-				className={`flex flex-col gap-12 fixed right-0 top-0 py-24 w-2/3 h-full transition-transform bg-white shadow-lg *:text-center *:transition-transform active:*:scale-95 sm:flex-row sm:static sm:w-full sm:shadow-none sm:translate-x-0 sm:px-0 sm:py-0 sm:bg-transparent text-lg ${
+				className={`flex flex-col gap-12 fixed right-0 top-0 z-50 py-24 w-2/3 h-full transition-transform bg-white shadow-lg *:text-center *:transition-[transform,color] active:*:scale-95 sm:transition-none sm:flex-row sm:static sm:w-full sm:shadow-none sm:translate-x-0 sm:px-0 sm:py-0 sm:bg-transparent text-lg ${
 					navOpen ? "translate-x-0" : "translate-x-full"
-				}`}>
-				<li className="">
-					<Link href="/about">About</Link>
-				</li>
-				<li>
-					<Link href="/games">Games</Link>
-				</li>
-				<li>
-					<Link href="https://github.com/VinctLY">Github</Link>
-				</li>
+				}`}
+				ref={navRef}>
+				<FontAwesomeIcon
+					icon={faXmark}
+					className="absolute top-4 right-8 size-6 cursor-pointer sm:hidden"
+					onClick={() => setNavOpen(false)}
+				/>
+				{links.map((link, i) => {
+					return (
+						<li className="hover:text-primary" key={i}>
+							<Link href={link.url}>{link.anchor}</Link>
+						</li>
+					);
+				})}
 			</div>
 		</ul>
 	);
